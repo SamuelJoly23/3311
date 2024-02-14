33087276a21873f41fd316d3f555499165d6ff46
SamuelJoly23 <samuel.joly@polymtl.ca>
2024-02-14 16:08:10 -0500

    MSA_SPI done
    Simulation du MSA_SPI

--- project_2_week1/project_2_week1.srcs/sources_1/imports/hdl/MSA_SPI.vhd
+++ project_2_week1/project_2_week1.srcs/sources_1/imports/hdl/MSA_SPI.vhd
@@ -13,70 +13,57 @@ entity MSA_SPI is
 end MSA_SPI;
 
 architecture Behavioral of MSA_SPI is
-type state_type is (attente, pret);
+type state_type is (attente, envoi);
 signal current_state: state_type;
   -- A completer
-signal cnt         : std_logic_vector (7 downto 0);
-signal ready       : std_logic; 
+signal cnt         : unsigned(6 downto 0);
+signal cs          : std_logic;
 signal data        : std_logic_vector (7 downto 0);
-signal start       : std_logic;
-
+signal clk_5Mhz    : std_logic;
+signal ready       : std_logic := '1'; 
+--signal start       : std_logic;
 
 begin
-process (ready, start, data)
+    sdo_o <= data(7);
+    clk_5Mhz <= '1' when (cnt mod 10) < 5 else '0';
+    sclk_o <= cs or clk_5Mhz;
+    cs <= '1' when current_state = attente else '0';
+    ready_o <= ready ;
+process (rst_i, clk_i)
 begin
 --  -- A remplacer
---  ready_o <= '1';
+    
 --  sdo_o   <= '0';
 --  sclk_o  <= '1';
-
 -- avoir un current state
 
 if rst_i = '1' then
     current_state <= attente;
-    -- cnt <= to_unsigned(0,cnt length);
+    cnt <= to_unsigned(0, cnt'length);
     data <= "00000000";
-else
+elsif(rising_edge(clk_i)) then
+
     case current_state is
         when attente =>
-            if(start = '1') then
-                --cnt <= to_unsigned(79, compteurlength);
+            if(start_i = '1') then
+                cnt <= to_unsigned(79, cnt'length);
                 data <= data_i;
-                current_state <= pret;
+                current_state <= envoi;
+                ready <= '0';
              else
                 current_state <= attente;
              end if;
-        when pret =>
+            
+        when envoi =>
+            ready <= '0';
             if(cnt = 0) then
                 current_state <= attente;
+                ready <= '1';
             elsif((cnt mod 10) = 0) then
                 data <= data(6 downto 0) & '0';
             end if;
             cnt <= cnt - 1;
          end case;
-      end if;
+     end if;
    end process;
-            
-
-
---if rising_edge(clk_i) then
---    case current_state is
---        when pret =>
---            if start = '1' then
---                if cnt = '0' then 
---                    cnt   <= std_logic_vector(unsigned(cnt) + 1);           
---                    data <= data_i;
---                if cnt = '1' then
---                    cnt = '0';
---                    current_state <= attente;
---                    ready_o <= '0';
---                end if;
---            end if; 
-            
---        when attente =>
---            if start = '0' then
-                
-        
---end if;
-end process;
 end Behavioral;
