852e140a51aa414ab42442d97b124a9f5cdb2af1
SamuelJoly23 <samuel.joly@polymtl.ca>
2024-02-17 13:36:00 -0500

    init_tb created

--- project_2_week1/project_2_week1.srcs/sim_1/new/init_tb_v2.vhd
+++ project_2_week1/project_2_week1.srcs/sim_1/new/init_tb_v2.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/16/2024 05:09:13 PM
--- Design Name: 
--- Module Name: init_tb_v2 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.numeric_std.all;
 
 entity init_tb_v2 is
     Port ( clk_i : in STD_LOGIC;
